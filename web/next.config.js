8f09d3e7d0c34b3e2877a501b442ff8e94df84fd
Ludson Almeida <ludson.bsa@gmail.com>
2025-10-01 20:10:54 -0300

    vai karai

--- web/next.config.js
+++ web/next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    return [
-      { source: '/api/:path*', destination: 'http://localhost:4000/:path*' },
-    ];
+    return [{ source: '/api/:path*', destination: 'http://localhost:4000/:path*' }];
   },
 };
 module.exports = nextConfig;
