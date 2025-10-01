4b319741ed37b877a4e6a733bb0baf6ab2c08ae7
Ludson Almeida <ludson.bsa@gmail.com>
2025-10-01 20:08:04 -0300

    simboraaaaa

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true }
+  async rewrites() {
+    return [
+      { source: '/api/:path*', destination: 'http://localhost:4000/:path*' },
+    ];
+  },
 };
 module.exports = nextConfig;
